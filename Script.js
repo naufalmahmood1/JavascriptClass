a849ad25c0ca8df5f65834147027f2adbd3ca165
naufalmahmood1 <naufalmahmood1@gmail.com>
2024-09-02 22:40:20 +0500

    Initial commit

--- Script.js
+++ Script.js
@@ -2,15 +2,17 @@ function add(a,b) {
 
     var a = 60;
     
-    var b = 60;
+    var b = 160;
     
     var c = a + b;
-    
+
     document.write(c);
+    console.log(c);
     
     }
     
-    add()
+
+    add();
 
 
 
